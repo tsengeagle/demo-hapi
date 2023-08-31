0b2c1f2ba48ab17feae433d29ee472b5cb65f912
tsengeagle <tsengeagle@gmail.com>
2023-08-31 23:42:22 +0800

    reference的操作

--- src/test/java/HapiTests.java
+++ src/test/java/HapiTests.java
@@ -2,8 +2,10 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.FhirVersionEnum;
 import ca.uhn.fhir.parser.IParser;
 import org.hl7.fhir.r4.model.Meta;
+import org.hl7.fhir.r4.model.Narrative;
 import org.hl7.fhir.r4.model.Observation;
 import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.utilities.xhtml.XhtmlNode;
 import org.junit.jupiter.api.Test;
 
 public class HapiTests {
@@ -13,7 +15,8 @@ public class HapiTests {
         observation.setMeta(new Meta().addProfile("https://twcore.mohw.gov.tw/fhir/StructureDefinition/Observation-laboratoryResult-twcore"));
 
         Patient patient = new Patient();
-
+        patient.setMeta(new Meta().addProfile("TEST"));
+        observation.getSubject().setResource(patient); // 新增reference
 
         FhirContext fhirContext = new FhirContext(FhirVersionEnum.R4);
         IParser jsonParser = fhirContext.newJsonParser().setPrettyPrint(true);
